de1cc108e073b7e33e4d3d8f2cf86ca561b3e056
Badot <bastien.badot@gmail.com>
2021-10-10 18:31:07 +0200

    Parallelisation du calcul de ligne et creation de la fonction line_compute

--- convolution.go
+++ convolution.go
@@ -60,22 +60,30 @@ func computeconvolution(result_array [][]int, image_agrandit [][]int, kernel [][
 	}
 }
 
+func line_compute(result_array [][]int, image_agrandit [][]int, kernel [][]int, y int, lenx int, leny int, nb_ligne int) {
+	for i := y; i < y+nb_ligne || i < leny; i++ {
+		for j := 0; j < lenx; j++ {
+			computeconvolution(result_array, image_agrandit, kernel, j, i)
+		}
+	}
+}
+
 func convolute(image_array [][]int, kernel [][]int) [][]int { //Fonction a appeler pour effectuer la convolution d'une image et d'un filtre
 	leny := len(image_array)
 	lenx := len(image_array[0])
 	image_agrandit := agrandit(image_array) //On traite l'image pour rajouter des 0 sur les bordures
 	result := slice2D(leny, lenx)
-	//maxGoroutines := 10000 Multiplie le temps de calcul par 2 donc pas interessant dans notre cas
+	nb_ligne := 10 //On définit le nombre de ligne que va calculer chaque go routine
+
+	//maxGoroutines := 10000
 	//guard := make(chan int, maxGoroutines) // On crée un channel qui va limiter le nombre de go routine qui tourneront en même temps
 
-	for i := 0; i < leny; i++ {
-		for j := 0; j < lenx; j++ {
-			//guard <- 1 //On rempli le channel lors du lancement de la routine
-			//go func(j int, i int) {
-			computeconvolution(result, image_agrandit, kernel, j, i) //Utiliser des go routines est plus long que de faire le calcul sans
-			//<-guard //On vide le channel à la fin de la tache
-			//}(j, i)
-		}
+	for i := 0; i < leny; i += nb_ligne {
+		//guard <- 1 //On rempli le channel lors du lancement de la routine
+		//go func(j int, i int) {
+		go line_compute(result, image_agrandit, kernel, i, lenx, leny, nb_ligne)
+		//<-guard //On vide le channel à la fin de la tache
+		//}(j, i)
 	}
 	return result
 }
@@ -83,7 +91,7 @@ func convolute(image_array [][]int, kernel [][]int) [][]int { //Fonction a appel
 func main() {
 	start := time.Now()
 
-	image := slice2D(3000, 3000) //Creation du 2D image pour tester
+	image := slice2D(1920, 1080) //Creation du 2D image pour tester
 	for i := 0; i < len(image); i++ {
 		for j := 0; j < len(image[0]); j++ {
 			image[i][j] = j + 10*i
