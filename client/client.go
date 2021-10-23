6d473b1938252ce1cc7738084b1bae4151654138
Badot <bastien.badot@gmail.com>
2021-10-23 19:08:24 +0200

    client : argument URL,outpoutPATH + serveur : implémentation de Sobel

--- client/client.go
+++ client/client.go
@@ -11,28 +11,32 @@ import (
 	"strconv"
 )
 
-func getArgs() int {
+func getArgs() (int, string, string) {
 
-	if len(os.Args) != 2 {
-		fmt.Printf("Usage: 	go run client.go <portnumber>\n")
+	if len(os.Args) != 4 {
+		fmt.Printf("Usage: 	go run client.go <serveur_portnumber> <URL_IMG> <destinationPath>\n")
 		os.Exit(1)
 	} else {
 		fmt.Printf("#DEBUG ARGS Port Number : %s\n", os.Args[1])
-		portNumber, err := strconv.Atoi(os.Args[1])
-		if err != nil {
-			fmt.Printf("Usage: go run client.go <portnumber>\n")
+		portNumber, err_port := strconv.Atoi(os.Args[1])
+		fmt.Printf("#DEBUG ARGS URL_IMG : %s\n", os.Args[2])
+		URL_IMG := os.Args[2]
+		fmt.Printf("#DEBUG ARGS destinationPath : %s\n", os.Args[3])
+		destinationPath := os.Args[3]
+		if err_port != nil {
+			fmt.Printf("Usage: go run client.go <serveur_portnumber> <URL_IMG> <destinationPath>\n")
 			os.Exit(1)
 		} else {
-			return portNumber
+			return portNumber, URL_IMG, destinationPath
 		}
 
 	}
 	//Should never be reached
-	return -1
+	return -1, "", ""
 }
 
 func writeImg(img *image.Gray, imgName string) error {
-	f, err := os.Create(imgName + ".png")
+	f, err := os.Create(imgName)
 	if err != nil {
 		return err
 	}
@@ -43,7 +47,7 @@ func writeImg(img *image.Gray, imgName string) error {
 }
 
 func main() {
-	port := getArgs()
+	port, url, destinationPath := getArgs()
 	fmt.Printf("#DEBUG DIALING TCP Server on port %d\n", port)
 	portString := fmt.Sprintf("127.0.0.1:%s", strconv.Itoa(port))
 	fmt.Printf("#DEBUG MAIN PORT STRING |%s|\n", portString)
@@ -53,13 +57,12 @@ func main() {
 		fmt.Printf("#DEBUG MAIN could not connect\n")
 		os.Exit(1)
 	} else {
-		url := "https://f.hellowork.com/blogdumoderateur/2013/02/nyan-cat-gif-1.gif"
 		io.WriteString(conn, url+"\n")
 		defer conn.Close()
 		decoder := gob.NewDecoder(conn)
 		var img image.Gray
 		decoder.Decode(&img)
-		writeImg(&img, "output")
+		writeImg(&img, destinationPath)
 	}
 
 }
---
+++ server/convolution_sobel.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"math"
+	"runtime"
+	"sync"
+)
+
+// Effectue la convolution pour 1 pixel
+func computeConvolutionSobel(resultArray [][]int16, imgAgrandie [][]int16, kernel1 [][]int16, kernel2 [][]int16, x int, y int) {
+	size := len(kernel1)
+	// On récupère une version size*size entourant le pixel que l'on veut traiter
+	cropedImage := crop(imgAgrandie, x, y, size)
+	result1 := int16(0)
+	result2 := int16(0)
+	//var temp1 float64
+	//var temp2 float64
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			// On effectue le calcul de la convolution on ajoutant les éléments opposés entre le filtre et l'image cropée
+			// On implémente la convolution de Sobel
+			//temp1 = math.Pow(float64(kernel1[i][j]*cropedImage[size-i-1][size-j-1]), 2)
+			//temp2 = math.Pow(float64(kernel2[i][j]*cropedImage[size-i-1][size-j-1]), 2)
+			result1 += kernel1[i][j] * cropedImage[size-i-1][size-j-1]
+			result2 += kernel2[i][j] * cropedImage[size-i-1][size-j-1]
+		}
+	}
+	resultArray[y][x] = int16(math.Sqrt(math.Pow(float64(result1), 2)+math.Pow(float64(result2), 2))) / 4
+}
+
+// Calcule la convolution sur un certain nombre de lignes
+func lineComputeSobel(resultArray [][]int16, imageAgrandie [][]int16, kernel1 [][]int16, kernel2 [][]int16, y int, lenX int, nbLigne int, waitGroup *sync.WaitGroup) {
+	for i := y; i < y+nbLigne; i++ {
+		for j := 0; j < lenX; j++ {
+			// Calcule la convolution pour un pixel
+			computeConvolutionSobel(resultArray, imageAgrandie, kernel1, kernel2, j, i)
+		}
+	}
+	waitGroup.Done()
+}
+
+// Fonction à appeler pour effectuer la convolution d'une image et d'un filtre
+func convoluteSobel(imageArray [][]int16, kernel1 [][]int16, kernel2 [][]int16) [][]int16 {
+	lenY := len(imageArray)
+	lenX := len(imageArray[0])
+	// On traite l'image pour rajouter des 0 sur les bordures
+	imageAgrandie := agrandie(imageArray)
+	result := slice2D(lenY, lenX)
+	// On définit le nombre de go routine max
+	var nbRoutine = runtime.NumCPU() * 2
+	// On rajoute 1 pour éviter les cas ou nbLigne est arrondi à l'inférieur
+	nbLigne := (lenY / nbRoutine) + 1
+	var waitGroup sync.WaitGroup
+
+	for i := 0; i < lenY; i += nbLigne {
+		waitGroup.Add(1)
+		// On vérifie qu'on ne dépasse pas le nombre de lignes max
+		if i+nbLigne > lenY {
+			go lineComputeSobel(result, imageAgrandie, kernel1, kernel2, i, lenX, lenY-i, &waitGroup)
+		} else {
+			go lineComputeSobel(result, imageAgrandie, kernel1, kernel2, i, lenX, nbLigne, &waitGroup)
+		}
+	}
+	waitGroup.Wait()
+	return result
+}
