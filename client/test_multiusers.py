3182f604479e30a229fa54f2b0ab848092dc6a69
Hugo THOMAS <thomas.hugo18@sfr.fr>
2021-11-18 16:41:36 +0100

    changed from .exe to go run

--- client/test_multiusers.py
+++ client/test_multiusers.py
@@ -3,7 +3,7 @@ import time
 from multiprocessing import Process
 
 def launch(i):
-    os.system("client.exe -D=img"+str(i)+" 5454 https://stsci-opo.org/STScI-01EVSVZHYGRC7KMTEJ6DJFXE4N.png")
+    os.system("go run client.go -D=img"+str(i)+" 5454 https://stsci-opo.org/STScI-01EVSVZHYGRC7KMTEJ6DJFXE4N.png")
 
 if __name__ == '__main__':
     for i in range(100):
