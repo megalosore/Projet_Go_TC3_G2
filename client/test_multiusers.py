53e4a42a779af76e5fca4e7dab1248d65ede3282
Hugo THOMAS <thomas.hugo18@sfr.fr>
2021-11-25 17:11:00 +0100

    change benchmark

--- client/test_multiusers.py
+++ client/test_multiusers.py
@@ -4,6 +4,6 @@ import time
 if __name__ == '__main__':
     processes = []
     for i in range(100):
-        processes.append(subprocess.Popen(["go", "run", "client.go", "-D=img"+str(i), "4312", "https://webypress.b-cdn.net/wp-content/uploads/2020/03/The-Ultimate-Coronavirus-Small-Business-Guide-WordPress-Tools.png"]))
+        processes.append(subprocess.Popen(["go", "run", "client.go", "-D=img"+str(i), "4312", "http://localhost/test.jpg"]))
 for p in processes:
     p.wait()
